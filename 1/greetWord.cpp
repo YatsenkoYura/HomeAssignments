1314e3d5b06a1f45fbf415e2180f01c34eb4bdc1
YatsenkoYura <st135624@student.spbu.ru>
2024-09-16 03:00:22 +0500

    update 1 project, add greetWord files

---
+++ 1/greetWord.cpp
@@ -0,0 +1,10 @@
+//
+// Created by KanoWare on 9/16/2024.
+//
+#include "greetWord.h"
+#include<iostream>
+
+using namespace std;
+void greetWord(string word){
+    cout << endl << "Hello, " << word << "!" << endl;
+}
---
+++ 1/greetWord.h
@@ -0,0 +1,9 @@
+//
+// Created by KanoWare on 9/16/2024.
+//
+#include<string>
+#ifndef INC_1_OUTPUTWORLD_H
+#define INC_1_OUTPUTWORLD_H
+
+void  greetWord(std::string word);
+#endif // INC_1_OUTPUTWORLD_H
