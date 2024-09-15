31b4f0137656784b34cae57a3152c259da97d2b1
YatsenkoYura <st135624@student.spbu.ru>
2024-09-16 03:07:45 +0500

    update 1 project, update main.cpp

--- 1/main.cpp
+++ 1/main.cpp
@@ -4,10 +4,17 @@ hello world project
  */
 
 #include <iostream>
-
+#include "greetWord.h"
 
 using namespace std;
-int main(){
 
-
+int main() {
+    cout << "Hello, World!" << endl;
+    string word;
+    while (true) {
+        cout << "Input: ";
+        getline(cin, word);
+        greetWord(word);
+    }
+    return 0;
 }
